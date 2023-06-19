8d529b9b37cdf2ed6deb8bfee531a5de3415cc30
bigsparsh <sparshsingh7586@gmail.com>
2023-06-19 10:54:51 +0530

    Implemented the plot graph with temporary mock data.

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ days = st.slider('Forecast Days: ',
                  max_value=5,
                  help='Select the number of forecasted days')
 option = st.selectbox('Select data to view',
-                       ('Temperature', 'Sky'))
+                      ('Temperature', 'Sky'))
 st.subheader(f'{option} for the next {days} days in {place}')
+
+dates = ['2023-06-12', '2023-07-12', '2023-08-12']
+temperatures = [20, 12, 23]
+
+figure = px.line(x=dates, y=temperatures, labels={'x': 'Dates',
+                                                  'y': 'Temperatures (C)'})
+st.plotly_chart(figure)
